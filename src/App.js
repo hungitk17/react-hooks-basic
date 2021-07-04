00014410169489ed0c9943493fbe1396c504b4e1
Trần Văn Hùng <hungitk17@gmail.com>
2021-07-04 22:48:13 +0700

    color box change color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
+import ColorBox from "./components/index.jsx";
 
 function App() {
   return (
     <div className="app">
         <h1>Welcom to react</h1>
+
+        <ColorBox />
     </div>
   );
 }
